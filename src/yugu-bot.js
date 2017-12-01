22b5ccba99a0ba688524807d5624429699d0f6aa
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-12-01 08:59:57 -0500

    Changes to yugu-bot.js.

--- src/yugu-bot.js
+++ src/yugu-bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.login('Your bot token here.');
 
 const imageFolder = './images/';
 const fs = require('fs');
@@ -18,34 +19,20 @@ client.on("ready", () => {
 client.on('message', message => {
     var sticker = message.content.toLowerCase();
     if (sticker.search("!") != -1) {
-        if (sticker.replace(/[^!]/g, "").length > 1) {
-            sticker = sticker.split("!");
-            for (var i = 0; i < sticker.length; i++) {
-                sticker[i] = sticker[i].slice(sticker[i].indexOf("!") + 1, sticker[i].length);
-                if (sticker[i].search(" ") != -1) {         
-                    sticker[i] = sticker[i].slice(0, sticker[i].indexOf(" "));                   
-                }
-                sticker[i].trim();
-                for (var checkerIndex = 0; checkerIndex < fileNameIndex; checkerIndex++) {
-                    if (sticker[i] == fileNameArray[checkerIndex]) {
-                        message.channel.sendMessage({ file: './images/' + fileNameArray[checkerIndex] + ".png"})
-                    }
-                }
+        sticker = sticker.split("!");
+        for (var i = 0; i < sticker.length; i++) {
+            sticker[i] = sticker[i].slice(sticker[i].indexOf("!") + 1, sticker[i].length);
+
+            if (sticker[i].search(" ") != -1) {
+                sticker[i] = sticker[i].slice(0, sticker[i].indexOf(" "));
             }
-        }
-        else {
-            sticker = sticker.slice(sticker.indexOf("!") + 1, sticker.length);
-            if (sticker.search(" ") != -1) {
-                sticker = sticker.slice(0, sticker.indexOf(" "));                
-            }
-            sticker.trim();
+            sticker[i].trim();
+
             for (var checkerIndex = 0; checkerIndex < fileNameIndex; checkerIndex++) {
-                if (sticker == fileNameArray[checkerIndex]) {
+                if (sticker[i] == fileNameArray[checkerIndex]) {
                     message.channel.sendMessage({ file: './images/' + fileNameArray[checkerIndex] + ".png" })
                 }
             }
         }
     }
 });
-
-client.login('Your token here');
